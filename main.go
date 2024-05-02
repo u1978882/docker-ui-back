9a1b060916fd69fa4fb3feb51f375e64f3d06c0f
u1978882 <126769186+u1978882@users.noreply.github.com>
2024-05-02 16:48:01 +0200

    millora en funcio de conneccio

--- main.go
+++ main.go
@@ -14,6 +14,39 @@ import (
 	"golang.org/x/crypto/ssh"
 )
 
+func executarComanda(serverAddress, username, password, command string) (string, error) {
+	// Configuración de la conexión SSH
+	sshConfig := &ssh.ClientConfig{
+		User: username,
+		Auth: []ssh.AuthMethod{
+			ssh.Password(password),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	// Realizar la conexión SSH
+	client, err := ssh.Dial("tcp", serverAddress, sshConfig)
+	if err != nil {
+		return "", fmt.Errorf("Error al conectar al servidor SSH: %v", err)
+	}
+	defer client.Close()
+
+	// Crear una nueva sesión SSH
+	session, err := client.NewSession()
+	if err != nil {
+		return "", fmt.Errorf("Error al crear la sesión SSH: %v", err)
+	}
+	defer session.Close()
+
+	// Ejecutar el comando en la sesión SSH
+	output, err := session.CombinedOutput(command)
+	if err != nil {
+		return "", fmt.Errorf("Error al ejecutar el comando en el servidor remoto: %v", err)
+	}
+
+	return string(output), nil
+}
+
 func main() {
 	app := pocketbase.New()
 
@@ -26,52 +59,15 @@ func main() {
 				return c.JSON(http.StatusForbidden, string("[]"))
 			}
 
-			// Configuración de la conexión SSH
-			sshConfig := &ssh.ClientConfig{
-				User: record.GetString("username"),
-				//User: "arnau",
-				Auth: []ssh.AuthMethod{
-					ssh.Password(record.GetString("pass")),
-					//ssh.Password("kaladin"),
-					// También puedes utilizar otros métodos de autenticación, como claves SSH, dependiendo de tu configuración
-				},
-				HostKeyCallback: ssh.InsecureIgnoreHostKey(),
-				// Otras configuraciones, como HostKeyCallback, pueden ser necesarias dependiendo de tu entorno.
-			}
-
-			// Dirección del servidor SSH (host:port)
 			serverAddress := fmt.Sprintf("%s:%s", record.GetString("ip"), strconv.Itoa(record.GetInt("port")))
 
-			// Comando a ejecutar en el servidor remoto
 			command := "docker images --format='{{json .}},'"
-			//docker images --format '{"Repository": "{{.Repository}}", "Tag": "{{.Tag}}", "ID": "{{.ID}}", "Created": "{{.CREATED}}"}'
 
-			// Realizar la conexión SSH
-			client, err := ssh.Dial("tcp", serverAddress, sshConfig)
+			output, err := executarComanda(serverAddress, record.GetString("username"), record.GetString("pass"), command)
 			if err != nil {
-				fmt.Printf("Error al conectar al servidor SSH: %v\n", err)
-				return c.JSON(http.StatusForbidden, string("[]"))
+				fmt.Printf("Error al ejecutar el comando SSH: %v\n", err)
+				return c.JSON(http.StatusForbidden, "[]")
 			}
-			defer client.Close()
-
-			// Crear una nueva sesión SSH
-			session, err := client.NewSession()
-			if err != nil {
-				fmt.Printf("Error al crear la sesión SSH: %v\n", err)
-				return c.JSON(http.StatusForbidden, string("[]"))
-			}
-			defer session.Close()
-
-			// Ejecutar el comando en la sesión SSH
-			output, err := session.CombinedOutput(command)
-			if err != nil {
-				fmt.Printf("Error al ejecutar el comando en el servidor remoto: %v\n", err)
-				return c.JSON(http.StatusForbidden, string("[]"))
-			}
-
-			// Imprimir la salida del comando
-			fmt.Println("Output del comando:")
-			fmt.Println(string(output))
 
 			var sortidaFinal = string(output)
 			if len(sortidaFinal) > 0 {
@@ -82,6 +78,39 @@ func main() {
 			fmt.Println(sortidaFinal)
 
 			return c.JSON(http.StatusOK, string("{\"images\": ["+sortidaFinal+"]}"))
+		} /* optional middlewares */)
+
+		return nil
+	})
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.GET("/functions/containers/:server", func(c echo.Context) error {
+			server := c.PathParam("server")
+
+			record, err := app.Dao().FindRecordById("server", server)
+			if err != nil {
+				return c.JSON(http.StatusForbidden, string("[]"))
+			}
+
+			serverAddress := fmt.Sprintf("%s:%s", record.GetString("ip"), strconv.Itoa(record.GetInt("port")))
+
+			command := "docker ps -a --format='{{json .}},'"
+
+			output, err := executarComanda(serverAddress, record.GetString("username"), record.GetString("pass"), command)
+			if err != nil {
+				fmt.Printf("Error al ejecutar el comando SSH: %v\n", err)
+				return c.JSON(http.StatusForbidden, "[]")
+			}
+
+			var sortidaFinal = string(output)
+			if len(sortidaFinal) > 0 {
+				sortidaFinal = sortidaFinal[:len(sortidaFinal)-2]
+			}
+
+			fmt.Println("Sortida final:")
+			fmt.Println(sortidaFinal)
+
+			return c.JSON(http.StatusOK, string("{\"contenidors\": ["+sortidaFinal+"]}"))
 		} /* optional middlewares */)
 
 		return nil
